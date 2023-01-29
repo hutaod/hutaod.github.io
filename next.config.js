6311662ad268f5a335bd6f3a1be0da8f9010cadb
hutao <1131589588@qq.com>
2023-01-29 08:24:24 +0800

    feat: add scripts cli and set config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  themeConfig: './theme.config.jsx'
 })
- 
-module.exports = withNextra()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // outDir: "docs",
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = withNextra(nextConfig)
  
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
